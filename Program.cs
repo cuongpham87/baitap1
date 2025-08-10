11903370c80b35a1d6ecd22584be69536275e3bf
cuongpham87 <lil.steven296@gmail.com>
2025-08-11 00:47:26 +0630

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-class Program{
+﻿class Program
+{
     public static void Main(string[] args)
     {
+        Console.WriteLine("hello the world");
         
     }
 }
